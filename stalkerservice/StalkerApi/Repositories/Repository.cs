f0abb172a6d247dbb0d3d6661468c24dd3cf66fb
Vikas <48914844+Vikas8881@users.noreply.github.com>
2025-03-04 14:23:36 +0530

    Add stalkerservice/StalkerApi/Repositories/Repository.cs 1741078415578

---
+++ stalkerservice/StalkerApi/Repositories/Repository.cs
@@ -0,0 +1,65 @@
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using StalkerModels.Data;
+using StalkerModels.Models;
+
+namespace StalkerApi.Repositories;
+
+public class Repository<T> : IRepository<T> where T : BaseEntity
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<T> AddAsync(T entity)
+    {
+        entity.CreatedAt = DateTime.UtcNow;
+        await _dbSet.AddAsync(entity);
+        await SaveChangesAsync();
+        return entity;
+    }
+
+    public virtual async Task<T> UpdateAsync(T entity)
+    {
+        entity.UpdatedAt = DateTime.UtcNow;
+        _context.Entry(entity).State = EntityState.Modified;
+        _context.Entry(entity).Property(x => x.CreatedAt).IsModified = false;
+        await SaveChangesAsync();
+        return entity;
+    }
+
+    public virtual async Task DeleteAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+            await SaveChangesAsync();
+        }
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
